0e3e80b4ac3360a547d31838293b3244e3e4feea
avermeulen <andre.vermeulen@gmail.com>
2016-03-04 11:50:22 +0200

    the weather example implemented

--- weather.js
+++ weather.js
@@ -1,3 +1,66 @@
-test( "hello test", function( assert ) {
-  assert.equal( 1, 2, "Passed!" );
-});
+
+function findPlaces(weatherString){
+
+    var places = weatherString
+        .split("in")
+        .slice(1);
+
+    var placeList = [];
+    for(var i=0;i<places.length;i++){
+        var place = places[i];
+
+        var placeString = place
+            .replace(",", "")
+            .replace("and", "")
+            .trim();
+
+        var placeParts = placeString.split(" ");
+
+        var placeName = placeParts[0];
+        if (isNaN(placeParts[1])){
+            placeName = placeParts[0] + " " + placeParts[1];
+        }
+        var temperature = Number(placeParts[placeParts.length-1]);
+
+        placeList.push({
+            place : placeName,
+            temperature : temperature
+        })
+    }
+
+    return placeList;
+
+}
+
+function findColdestPlace(weatherString){
+    var places = findPlaces(weatherString);
+
+    var coldestTemp = places[0].temperature;
+    var coldestPlace = "";
+
+    for(var i=0; i<places.length;i++){
+        var place = places[i];
+        if (place.temperature < coldestTemp ){
+            coldestTemp = place.temperature;
+            coldestPlace = place.place;
+        }
+    }
+    return coldestPlace;
+}
+
+function findHottestPlace(weatherString){
+
+    var places = findPlaces(weatherString);
+
+    var highestTemp = 0;
+    var hottestPlace = "";
+    //find the hottest place
+    for(var i=0; i<places.length;i++){
+        var place = places[i];
+        if (place.temperature > highestTemp ){
+            highestTemp = place.temperature;
+            hottestPlace = place.place;
+        }
+    }
+    return hottestPlace;
+}
