e7da5963e33bbe94f41a7886ddb6ffac3231422a
avermeulen <andre.vermeulen@gmail.com>
2016-03-04 14:27:24 +0200

    ...

--- weather_tests.js
+++ weather_tests.js
@@ -1,18 +1,18 @@
-test( "find the hottest", function( assert ) {
+QUnit.test( "find the hottest", function( assert ) {
 
     var hottestPlace = findHottestPlace("Hi Joe, look at this tomorrow it will be really cold all over the country; in Cape Town 9, in George 7, in Port Elizabeth 5 and in Johannesburg -1")
 
     assert.equal( "Cape Town", hottestPlace);
 });
 
-test( "find the coldest", function( assert ) {
+QUnit.test( "find the coldest", function( assert ) {
 
     var coldestPlace = findColdestPlace("Hi Joe, look at this tomorrow it will be really cold all over the country; in Cape Town 9, in George 7, in Port Elizabeth 5 and in Johannesburg 1")
 
     assert.equal( "Johannesburg", coldestPlace);
 });
 
-test( "find places", function( assert ) {
+QUnit.test( "find places", function( assert ) {
 
     var places = findPlaces("Hi Joe, look at this tomorrow it will be really cold all over the country; in Cape Town 9, in George 7, in Port Elizabeth 5 and in Johannesburg 1")
 
